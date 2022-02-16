4614991b3642b5660fdb7bc9ddfdb7bc8aa0a99a
emwadun <duncan.owino@gmail.com>
2022-02-16 19:00:12 +0000

    updates

--- 1_sequential.py
+++ 1_sequential.py
@@ -1,9 +1,9 @@
 #sequential execution of code
-#if we coment out declared variable a, we will get !NameError: name 'a' is not defined
+#if we coment out declared variable a, we will get "NameError: name 'a' is not defined"
 #since it is required in the execution
 
 a = 40 
 b = 20
 c = a - b
 
-print("Subtraction is :", c)
+print("Subtraction is :", c) #This will print 20
